ae5c910aaa45858f3a966abdf416f8ff2a71faf1
berdibekkurbanbaev <kurbanbaevberdibek@gmail.com>
2022-05-10 18:35:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        list.stream().filter(x->x%2==1).forEach(System.out::println);
+
+
+
+    }
+}
